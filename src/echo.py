77bdccf19e08c61c74764152e9d9221da934fd37
Sara-Elna <elna.elisa@gmail.com>
2022-09-13 13:32:27 +0200

    Ed works, part of cat works, echo does not work

--- src/echo.py
+++ src/echo.py
@@ -1,3 +1,3 @@
 import sys
 
-print(' '.join(sys.argv))
+print(' '.join(sys.argv[1:]))
---
+++ src/ed.py
@@ -0,0 +1,5 @@
+while True:
+    if input() != 'eat flaming death':
+        print('?')
+    else:
+        break
